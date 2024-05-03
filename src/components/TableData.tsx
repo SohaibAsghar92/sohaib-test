3ac8cb59ec1c3297dc96e83bc8276959d7c78a84
sohaibasghar92 <asgharsohaib92@gmail.com>
2024-05-03 21:25:29 +0500

    lint issues fixed

--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -1,14 +1,11 @@
-import React, { FC } from 'react'
+import { FC } from "react";
 
 interface Props {
-    value: string
+  value: string;
 }
-
 
 const TableData: FC<Props> = ({ value }) => {
-    return (
-        <td className='border border-slate-700'>{value}</td>
-    )
-}
+  return <td className="border border-slate-700">{value}</td>;
+};
 
-export default TableData
+export default TableData;
